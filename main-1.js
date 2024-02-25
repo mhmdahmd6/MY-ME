61c0f0982b4c30d3afe85290f72c466665e7719a
mhmdahmd6 <150290045+mhmdahmd6@users.noreply.github.com>
2024-02-25 15:26:23 +0200

    Add files via upload

---
+++ main-1.js
@@ -0,0 +1,10 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '15px',
+    duration: 2500,
+    delay: 400
+})
+
+ScrollReveal().reveal('.tagline ,.section__text__p1' , { delay: 500, origin: 'left'});
+ScrollReveal().reveal('.section-box ,.title', { delay: 600, origin: 'bottom'});
+ScrollReveal().reveal('.text-box ', { delay: 700, origin: 'right'});
